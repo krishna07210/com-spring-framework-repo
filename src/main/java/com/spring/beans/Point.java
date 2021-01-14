f7662d9b21b3239da3bf2c14946a041aab450183
Harikrishna Thariboyina <=>
2021-01-14 13:37:41 +0530

    spring framework basics

---
+++ src/main/java/com/spring/beans/Point.java
@@ -0,0 +1,30 @@
+package com.spring.model;
+
+public class Point {
+    int x;
+    int y;
+
+    Point() {
+        System.out.println("Constructor will automatically called when bean is created");
+    }
+
+    Point(String myString, float myFloat, int myInt, double myDouble) {
+        System.out.println("myString : " + myString + " myFloat : " + myFloat + " myInt : " + myInt + " myDouble : " + myDouble);
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
